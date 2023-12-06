443429f99ac6d9723aa159b945cce0b9d100fcc8
devboss0924 <smallstar0924@gmail.com>
2023-12-07 03:04:36 +0400

    completed

--- Helloalma.py
+++ Helloalma.py
@@ -1,23 +1,6 @@
 import csv
-import threading
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import undetected_chromedriver as uc
-import pandas as pd
 import time
-import re
-import csv
-from datetime import datetime
-from datetime import date
 from seleniumbase import Driver
 
 driver = Driver(uc=True)
@@ -25,7 +8,7 @@ pageCount = 1
 with open('URL.csv', mode='r') as file:
     reader = csv.reader(file)
     for row in reader:
-        print("------------------------", pageCount, "-----------------------")
+        print("--------------------", pageCount, "---------------------")
         pageCount += 1
         Region = row[0]
         driver.get(row[1])
@@ -36,31 +19,26 @@ with open('URL.csv', mode='r') as file:
         match=False
         while(match==False):
             lastCount = lenOfPage
-            time.sleep(3)
+            time.sleep(5)
             lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
             if lastCount==lenOfPage:
                 match=True
-        ############################
-        # print("the end")
+
         provider_links = []
         provider_elements = driver.find_elements(By.XPATH, '//*[@id="maincontent"]/div[2]/div/article/a')
         for provider_element in provider_elements:
             provider_link = provider_element.get_attribute("href")
-            # print("provider link >>> ", provider_link)
             provider_links.append(provider_link)
         for provider_link in provider_links:
             driver.get(provider_link)
             
-            while(True):
-                try:
-                    pageTitle = driver.find_element(By.XPATH, '//*[@id="providerProfile"]/div[1]/div/div[2]/div[1]').text
-                    break
-                except:
-                    driver.refresh()
-
-            name = pageTitle.split("\n")[0]
-
-            title = pageTitle.split("\n")[1]
+            try:
+                pageTitle = driver.find_element(By.XPATH, '//*[@id="providerProfile"]/div[1]/div/div[2]/div[1]').text
+                name = pageTitle.split("\n")[0]
+                title = pageTitle.split("\n")[1]
+            except:
+                name = "No data"
+                title = "No data"
 
             try:
                 Bio = driver.find_element(By.XPATH, '//*[@id="providerProfile"]/div[1]/div/div[2]/div[3]/p').text
@@ -93,6 +71,7 @@ with open('URL.csv', mode='r') as file:
                 Pay_out_of_pocket = driver.find_elements(By.CSS_SELECTOR, 'div.m_out-of-pocket>ul>li:first-child>div>span:nth-child(2)>div:nth-child(2)')[0].get_attribute('innerText')
             
                 Min_Range = Pay_out_of_pocket.split("-")[0]
+
                 Max_Range = "$" + Pay_out_of_pocket.split("-")[1]
             except:
                 Pay_out_of_pocket = "No data"
@@ -105,11 +84,11 @@ with open('URL.csv', mode='r') as file:
                 Licensed_in = "No data"
 
             headers = ["State USED", "Profile URL", "Name", "Title", "Bio", "Specialties", "Pay with insurance", "Accepted programs", "Pay out of pocket", "Min Range", "Max Range", "Licensed in"]
-            results = [Region, provider_link ,name, title, Bio, specialties, Pay_with_insurance, Accepted_programs, Pay_out_of_pocket, Min_Range, Max_Range, Licensed_in]
+            results = [Region, provider_link, name, title, Bio, specialties, Pay_with_insurance, Accepted_programs, Pay_out_of_pocket, Min_Range, Max_Range, Licensed_in]
             
             # Open the file in append mode
             with open('clients.csv', mode='a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerow(results)
-            print("write!!!")
+            print("Write success!!!")
 driver.close()    
